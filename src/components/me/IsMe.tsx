56a51ee85784a8b4089575df31a4f9e66e8afaf3
linzheng <alidlg9045@126.com>
2024-04-14 00:25:03 +0800

    feat: update avatar

--- src/components/me/IsMe.tsx
+++ src/components/me/IsMe.tsx
@@ -3,7 +3,7 @@ import { Avatar } from '@radix-ui/themes';
 export default function IsMeComp() {
   return (
     <div>
-      <Avatar src="/images/avatar.jpg" fallback="Avatar" size="8" radius="full" />
+      <Avatar src="/images/avatar.jpg" fallback="Avatar" size="9" radius="full" />
     </div>
   )
 };
