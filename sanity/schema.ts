a6c4d3f5630d6bbbe3c06ee0803df7317d73dd9d
linzheng <alidlg9045@126.com>
2024-04-20 17:01:16 +0800

    install sanity to nextjs

---
+++ sanity/schema.ts
@@ -0,0 +1,9 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+import blockContent from './schemaTypes/blockContent'
+import category from './schemaTypes/category'
+import post from './schemaTypes/post'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [post, category, blockContent],
+}
---
+++ sanity/schemaTypes/post.ts
@@ -0,0 +1,74 @@
+import { defineField, defineType } from 'sanity'
+import { PencilSwooshIcon } from '~/src/assets/icons';
+
+export default defineType({
+  name: 'post',
+  title: '文章',
+  type: 'document',
+  icon: PencilSwooshIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      title: '标题',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: '链接标识符',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      title: '主图',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        }
+      ]
+    }),
+    defineField({
+      name: 'categories',
+      title: '分类',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+    }),
+    defineField({
+      name: 'body',
+      title: '内容',
+      type: 'blockContent',
+    }),
+    defineField({
+      name: 'description',
+      title: '简介',
+      type: 'text',
+      rows: 3
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: '发布时间',
+      type: 'datetime',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const { author } = selection
+      return { ...selection, subtitle: author && `by ${author}` }
+    },
+  },
+})
