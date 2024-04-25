a81793d559c1831e16b2c565fe986809db9e63a2
linzheng <alidlg9045@126.com>
2024-04-25 23:36:22 +0800

    feat: install @portabletext/react

--- src/app/(site)/blogs/[blog]/page.tsx
+++ src/app/(site)/blogs/[blog]/page.tsx
@@ -1,15 +1,26 @@
 import { getBlog } from '~/sanity/query/post';
+import { PortableText } from '@portabletext/react';
+
 interface Props {
   params: { blog: string }
 }
 export default async function Blog({ params }: Props) {
   const slug = params.blog
-  
+
   const blog = await getBlog(slug)
-  
-  
+
+
+
   return (
-    <div>{blog.description}</div>
+    <div>
+      {
+        blog.body && (
+          <PortableText
+            value={blog.body}
+          />
+        )
+      }
+    </div>
   )
 
 };
