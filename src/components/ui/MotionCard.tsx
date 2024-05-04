7ba40cb01ddaa36d24221c383a23be26ad1bfd32
linzheng <alidlg9045@126.com>
2024-05-04 11:53:48 +0800

    feat: change card motion

--- src/components/ui/MotionCard.tsx
+++ src/components/ui/MotionCard.tsx
@@ -8,23 +8,24 @@ import { type Post } from "~/sanity/types";
 
 export default function MotionCard({ post }: { post: Post }) {
   const { slug, mainImage, title, categories, description } = post
-  const MotionLink = motion(Link)
 
   return (
-    <MotionLink
-      id={slug}
-      key={slug}
-      href={'#' + slug}
-      initial={{ x: 100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: 1 }}
-      whileInView={{ opacity: 1, x: 0 }}
-    >
+    <Link id={slug} key={slug} href={'#' + slug}>
       <div className='flex flex-wrap'>
-        <div className='w-full lg:w-1/4 mr-4'>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -40 }}
+          transition={{ duration: 1 }}
+          className='w-full lg:w-1/4 mr-4'
+        >
           <Image src={mainImage} width={220} height={220} alt={title} className='rounded' />
-        </div>
-        <div className="w-full max-w-xl lg:3/4 flex flex-col justify-between">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 80 }}
+          transition={{ duration: 1 }}
+          className="w-full max-w-xl lg:3/4 flex flex-col justify-between"
+        >
           <div>
             <h6 className='mb-2 font-semibold' >{title}</h6>
             <p className='mb-2 text-neutral-400'>{description}</p>
@@ -36,8 +37,8 @@ export default function MotionCard({ post }: { post: Post }) {
               )
             }
           </div>
-        </div>
+        </motion.div>
       </div>
-    </MotionLink>
+    </Link>
   )
 };
