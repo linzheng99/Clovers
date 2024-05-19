a0891309906629ef4ac6ac0ed80ce57d63d00ab0
linzheng <alidlg9045@126.com>
2024-05-19 15:57:04 +0800

    feat: lifestyle -> life

--- config/nav.ts
+++ config/nav.ts
@@ -2,6 +2,6 @@ export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
   { href: '/projects', text: '项目' },
-  { href: '/lifestyle', text: '生活方式' },
+  { href: '/life', text: '生活方式' },
   { href: '/about', text: '关于' },
 ]
