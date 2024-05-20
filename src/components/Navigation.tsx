6d9b5e516cc35109f94282e65f4318e1f49d2b77
linzheng <alidlg9045@126.com>
2024-05-20 23:46:12 +0800

    feat: 小屏幕的菜单

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -23,7 +23,7 @@ export default function NavigationComp() {
       <nav
         className="max-w-screen-2xl relative mx-2 w-full rounded-[36px] items-center border px-4 py-3 border-neutral-700/40 bg-neutral-800/80 backdrop-blur-md md:flex md:items-center md:justify-between md:px-6 md:py-0 lg:px-8 xl:mx-auto"
       >
-        <div className="flex items-center justify-between h-full">
+        <div className="flex items-center justify-between h-[38px]">
           <Link href="/">
             <span>{appConfig.site}</span>
           </Link>
@@ -37,11 +37,14 @@ export default function NavigationComp() {
             </div>
           </div>
         </div>
-        <div className="hidden grow basis-full overflow-hidden transition-all duration-300 md:block">
+        <div
+          className={
+            cn('grow basis-full overflow-hidden transition-all duration-300 md:block', !toggle ? 'hidden' : '')
+          }>
           <div className="mt-5 flex flex-col gap-x-0 gap-y-4 md:mt-0 md:flex-row md:items-center md:justify-end md:gap-x-4 lg:gap-x-7 md:gap-y-0 md:ps-7">
             {links.map(link =>
-              <Link href={link.href} key={link.href} className="rounded-lg text-base font-medium outline-none  focus-visible:ring text-neutral-400 ring-zinc-200 hover:text-neutral-500 focus:outline-none md:py-3 md:text-sm 2xl:text-base">
-                <span className={cn(isActivate(link.href) ? 'text-[#d65fcf]': '')}>
+              <Link href={link.href} key={link.href} onClick={() => setToggle(false)} className="rounded-lg text-base font-medium outline-none  focus-visible:ring text-neutral-400 ring-zinc-200 hover:text-neutral-500 focus:outline-none md:py-3 md:text-sm 2xl:text-base">
+                <span className={cn(isActivate(link.href) ? 'text-[#d65fcf]' : '')}>
                   {link.title}
                 </span>
               </Link>)}
